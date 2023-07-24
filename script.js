07eb3087c208b6272b8e0bdc88bcd6b495a408e6
luizh3nrique <luizh3nrique00@gmail.com>
2023-07-24 13:48:18 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    if (window.scrollY > 200) {
-        header.classList.add("scroll");
-    } else {
-        header.classList.remove("scroll");
-    }
-});
